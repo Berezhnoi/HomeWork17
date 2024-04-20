2842bb452d891d0b800fab6fd3b6f50a71d79391
IgorBerezhnoi <iberezhnoi@qualium-systems.com>
2024-04-20 15:56:29 +0200

    Create Project

---
+++ Homework17/Homework17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework17
+//
+//  Created by rendi on 20.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
